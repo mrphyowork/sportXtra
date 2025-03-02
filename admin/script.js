de9d324e174c53d3ba0e43523ea3828154b229e5
Aung Phyo Thu <aungphyothu@Aungs-MacBook-Pro.local>
2025-03-02 16:19:34 +0700

    final edited

---
+++ admin/script.js
@@ -0,0 +1,98 @@
+// For Clock
+function updateClock() {
+  let date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  const day = days[date.getDay()];
+  const d = date.getDate();
+  const month = months[date.getMonth()];
+
+  const clockElement = document.getElementById("clockTime");
+  clockElement.textContent = `  ${hours}:${minutes}:${seconds} `;
+
+  const dateElement = document.getElementById("clockDate");
+  dateElement.textContent = `${day} ${d} ${month}`;
+}
+setInterval(updateClock, 1000);
+updateClock();
+
+// To check validation
+const loginButton = document.getElementById("loginButton");
+loginButton.addEventListener("click", function () {
+  const mainBody = document.getElementById("mainBody");
+  const formBody = document.getElementById("formBody");
+
+  const errorBox = document.getElementById("error");
+
+  const userName = document.getElementById("userName");
+  const password = document.getElementById("password");
+  // console.log(userName.value);
+  // console.log(password.value);
+
+  let messageBox = [];
+
+  if (userName.value === "" || userName.value === null) {
+    messageBox.push("- Username is required!");
+  } else if (userName.value != "admin") {
+    messageBox.push("-  Username is invalid!");
+  }
+
+  if (password.value === "" || password.value === null) {
+    messageBox.push("- Password is required!");
+  } else if (password.value != "admin") {
+    messageBox.push("- Password is incorrect!");
+  }
+  // console.log(messageBox.length);
+
+  if (messageBox.length > 0) {
+    // e.preventDefault();
+    errorBox.innerHTML = messageBox.join("<br>");
+    errorBox.style.display = "block";
+  } else {
+    mainBody.classList.remove("hidden");
+    formBody.classList.add("hidden");
+  }
+});
+
+// To Reset
+const resetBtn = document.getElementById("resetBtn");
+resetBtn.addEventListener("click", function () {
+  const userName = document.getElementById("userName");
+  const password = document.getElementById("password");
+
+  userName.value = "";
+  password.value = "";
+});
+
+// To Logout
+const logout = document.getElementById("logout");
+logout.addEventListener("click", function () {
+  const errorBox = document.getElementById("error");
+  const userName = document.getElementById("userName");
+  const password = document.getElementById("password");
+
+  errorBox.style.display = "none";
+  userName.value = "";
+  password.value = "";
+
+  mainBody.classList.add("hidden");
+  formBody.classList.remove("hidden");
+});
